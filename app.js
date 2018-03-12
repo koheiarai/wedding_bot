cdb0b86e4042e5563874f61030a13b068b5dd3ad
EC2 Default User <ec2-user@ip-172-31-12-227.ap-southeast-1.compute.internal>
2018-03-12 06:50:57 +0000

    12th Commit.

--- app.js
+++ app.js
@@ -79,16 +79,3 @@ const _delegateAi = function(req) {
   }
   return res
 }
-
-// var express        = require( 'express' );
-// var http           = require( 'http' );
-
-// var app            = express();
-// app.set( 'port', process.env.PORT || 3001 );
-// app.get('/', function (req, res) {
-//     res.send('Hello World');
-// });
-
-// http.createServer( app ).listen( app.get( 'port' ), function (){
-//   console.log( 'Express server listening on port ' + app.get( 'port' ));
-// });
