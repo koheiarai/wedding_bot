f7e4d37092db4f123aae7ee31f54d6f7226159a3
EC2 Default User <ec2-user@ip-172-31-12-227.ap-southeast-1.compute.internal>
2018-03-12 08:33:35 +0000

    25th Commit.

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 // event handler
 function handleEvent(event) {
   // if (event.type !== 'message' || event.message.type !== 'text' || event.message.type !== 'image') {
-  if (event.type !== 'message' || event.message.type !== 'text' || event.message.type !== 'image') {
+  if (event.type !== 'message' || (event.message.type !== 'text' && event.message.type !== 'image') ) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
