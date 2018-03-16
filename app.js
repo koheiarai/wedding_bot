95bb3c546f2b6135774e19da0594ff95070f5fce
kohei.arai <goahead.arai@gmail.com>
2018-03-16 15:33:21 +0000

    40th Commit.

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ const PORT = 3000;
 const IP = "localhost";
 const line = require('@line/bot-sdk');
 const extend = require('extend');
-const send = require('wedding/http/send-request');
-const templatePath = 'wedding/template/app/line/';
 const fs = require('fs');
+const request = require('request');
 
 // Move to config file
 let photos = [
