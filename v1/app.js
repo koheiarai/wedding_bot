0a8b967fa8281ed24b9049ed31a56b220ccfdd88
EC2 Default User <ec2-user@ip-172-31-12-227.ap-southeast-1.compute.internal>
2018-03-16 14:23:01 +0000

    28th Commit.

---
+++ v1/app.js
@@ -0,0 +1,143 @@
+// Defining the libraries
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 3000;
+const IP = "localhost";
+const line = require('@line/bot-sdk');
+
+// Move to config file
+let photos = [
+  {name: "Pomsky", image: "http://www.pomsky.org/wp-content/uploads/2013/03/f14629e88cfe11e28ef622000a1fa434_7.jpg"},
+  {name: "Cute Dog", image: "https://static.pexels.com/photos/356378/pexels-photo-356378.jpeg"},
+  {name: "Cute Dog", image: "https://static.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg"},
+  {name: "Cute Dog", image: "https://static.pexels.com/photos/406014/pexels-photo-406014.jpeg"}
+]
+
+// Line config
+// ToDo: Move to 
+const defaultAccessToken = 'pY08L55FaKvaHFayaOdPDySWBMQgJ0m6TvhjWa/axFQksupY7VvxIyuKA4EZ5geFFq6IzwmGD/zJSGtQulaLwsv3z79Ek7gs0MXgbuPbEH62+qkQcjdhFyMm/GJR/pwB4RuXWvb0klGJbfSBxM2iAgdB04t89/1O/w1cDnyilFU=';
+const defaultSecret = 'c71eb556eadc94d7e8bdeee14d9f08db';
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || defaultAccessToken,
+  channelSecret: process.env.CHANNEL_SECRET || defaultSecret,
+};
+
+
+// Setting view engines
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Welcome photosMessage
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+// Get the result of compounds
+app.get("/photos", function(req, res){
+  res.render("photos", {photos: photos});
+});
+
+// Getting the score of an image
+app.post("/photos", function(req, res){
+  const name = req.body.name;
+  const image = req.body.image;
+    // const score = Math.floor(Math.random() * 100) + " points";
+  const score = _delegateAi(req);
+  const newPhoto = {name: name, image: image, score: score};
+  photos.push(newPhoto);
+  res.redirect("/photos");
+});
+
+// Page to create a new page
+app.get("/photos/new", function(req, res){
+  res.render("new.ejs");
+});
+
+// Defining the port number and IP address
+app.listen(process.env.PORT || PORT, function(){
+  console.log("Server has started!")
+});
+
+// .listen(process.env.PORT || 5000)
+
+// Have a function of each AI depending on the input.
+// Move to another file and that file is referred to as the master logic file.
+// ToDo: Allow line app to send an image to the web application
+const _delegateAi = function(req) {
+  let res = "";
+  switch(req.body.option){
+    case "1":
+      // res = "One!";
+      res = Math.floor(Math.random() * 100) + " points";
+      break;
+    case "2":
+      // res = "Two!";
+      res = Math.floor(Math.random() * 100) + " points";
+      break;
+    case "3":
+      // res = "Three!";
+      res = Math.floor(Math.random() * 100) + " points";
+      break;
+    case "4":
+      // res = "Four!";
+      res = Math.floor(Math.random() * 100) + " points";
+      break;
+    case "5":
+      // res = "Five!";
+      res = Math.floor(Math.random() * 100) + " points";
+      break;
+  }
+  return res
+}
+
+// create LINE SDK client
+// ToDo: move below to another file later
+const client = new line.Client(config);
+
+// register a webhook handler with middleware
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+// event handler
+// https://api.line.me/v2/bot/message/{messageId}/content
+// event.message.id should be filled here to get the Image content 
+function handleEvent(event) {
+  // if (event.type !== 'message' || event.message.type !== 'text' || event.message.type !== 'image') {
+  if (event.type !== 'message' || (event.message.type !== 'text' && event.message.type !== 'image') ) {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+  
+  // create a echoing text message
+  let echo = { type: 'text', text: `「${event.message.text}」ではなくて画像を送ってください。\nBy新婦` };
+  if (event.message.type === 'image') {
+    echo = { type: 'text', text: "Thank you for your image!" };
+  }
+  
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// ToDo: Write the function to get an image
+// function getContent(messageId) {
+//   // let options = require(templatePath + 'GET_CONTENT.json'); ToDo: a_change
+//   let options = JSON.parse(fs.readFileSync(templatePath + 'GET_CONTENT.json', 'UTF-8'));
+//   const data = {
+//     'url': this.config['BASE_URL'] + this.config['GET_CONTENT_PATH_FIRST'] + "/" + messageId + "/" + this.config['GET_CONTENT_PATH_SECOND'],
+//     'headers': {
+//         'Authorization': 'Bearer ' + this.config['CHANNEL_ACCESS_TOKEN']
+//     },
+//     'encoding': null
+//   };
+//     extend(true, options, data);
+//     const response = yield send(options); // リクエスト
+//     response.validateStatusCodes(200);
+//     var buffer = new Buffer(response.getBody());
+//     return buffer; // バイナリデータをreturn
+// }
